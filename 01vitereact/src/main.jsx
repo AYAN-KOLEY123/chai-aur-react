c617f79cbb0329eb756ff1bfac1672dcacc8de21
AYAN-KOLEY123 <ayankoleybidya@gmail.com>
2026-01-20 09:56:45 +0530

    Changes

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,31 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 function MyApp() {
   return (
     <div>
-      <h1>Custom App!</h1>
+      <h1>Custom App | Chai</h1>
     </div>
   );
 }
-createRoot(document.getElementById("root")).render(<MyApp />);
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit Google
+//   </a>
+// );
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to visit google",
+);
+createRoot(document.getElementById("root")).render(reactElement);
