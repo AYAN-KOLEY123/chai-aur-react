0d38b1d439ba9cae4e5430fbe83ed71e280b350a
AYAN-KOLEY123 <ayankoleybidya@gmail.com>
2026-01-20 07:28:42 +0530

    Changes

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,3 +1,11 @@
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHtml = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+}
+
 const reactElement = {
   type: "a",
   props: {
@@ -7,3 +15,4 @@ const reactElement = {
   children: "Click me to visit google",
 };
 const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer);
