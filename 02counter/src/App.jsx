831d9a5c51488a0a915136a9916feb68566401a3
AYAN-KOLEY123 <ayankoleybidya@gmail.com>
2026-01-22 09:18:26 +0530

    Changes

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,21 +1,33 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  let counter = 14;
+  let [counter, setCounter] = useState(0);
+  // let counter = 14;
   const addValue = () => {
-    console.log("Value added", Math.random());
+    //counter = counter + 1;
+    //console.log("Clicked", counter);
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const removeValue = () => {
+    // console.log("Clicked", counter);
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
   };
   return (
     <>
       <h1>Chai Aur React</h1>
       <h2>Counter Value: {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button onClick={addValue}>Add value {counter}</button>
       <br />
-      <button>Remove value</button>
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>Footer: {counter}</p>
     </>
   );
 }
