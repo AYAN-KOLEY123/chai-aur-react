1c17523296f2e3f9acb88a8aba67ac99ff4c6d03
AYAN-KOLEY123 <ayankoleybidya@gmail.com>
2026-01-25 19:02:29 +0530

    Changes

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,18 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   //const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-green-400 text-black p-4 rounded-b-4xl">
+      <h1 className="bg-green-400 text-black p-4 rounded-b-4xl mb-4">
         Tailwind Test
       </h1>
+      <Card />
+      <Card />
     </>
   );
 }
