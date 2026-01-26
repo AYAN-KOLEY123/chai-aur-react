1b2e489204b74140a155ab3b32d82811c908c633
AYAN-KOLEY123 <ayankoleybidya@gmail.com>
2026-01-26 08:17:38 +0530

    Change

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,10 +10,10 @@ function App() {
     //counter = counter + 1;
     //console.log("Clicked", counter);
     if (counter < 20) {
-      setCounter(counter + 1);
-      setCounter(counter + 1);
-      setCounter(counter + 1);
-      setCounter(counter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
+      setCounter((prevCounter) => prevCounter + 1);
     }
   };
   const removeValue = () => {
